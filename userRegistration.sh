8327982c90a8af5b4a84d8d6b433b1754d0c870c
rishibodake <rishibodake@gmail.com>
2020-03-17 10:16:57 +0530

    Added:CheckForLastName

--- userRegistration.sh
+++ userRegistration.sh
@@ -7,7 +7,7 @@
 checkUserName(){
 
 	read -p "Enter UserName: " userName
-	pattern="^[A-Z][a-z]{2,}"
+	local pattern="^[A-Z][a-z]{2,}"
 	if [[ $userName =~ $pattern ]]
 	then
 		echo Valid
@@ -15,5 +15,15 @@ checkUserName(){
 		echo InValid
 	fi
 }
-checkUserName
+checkLastName(){
+	read -p "Enter LastName: " lastName
+	local pattern="^[A-Z][a-z]{2,}"
+	if [[ $lastName =~ $pattern ]]
+	then
+		echo Valid LastName
+	else
+		echo InValid LastName
+	fi
+}
+checkLastName
 
